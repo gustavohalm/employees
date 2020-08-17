1d8ff9c87557f34f32f0a3574e903fa35aadc766
gustavo <contato@gustavohalm.com>
2020-08-16 22:07:23 -0300

    fix serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
+from .models import Employe
 
 
 class EmployeSerializer(serializers.ModelSerializer):
-
     class Meta:
+        model = Employe
         fields = '__all__'
